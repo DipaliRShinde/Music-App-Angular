ac269387a7e4e472bbf297afe3560e691d7e25c5
DipaliRShinde <drs1454006@gmail.com>
2019-06-11 18:42:22 +0530

    details added

--- src/app/music.service.ts
+++ src/app/music.service.ts
@@ -18,7 +18,7 @@ export class MusicService {
     }
 
     getTrack(value):any {
-      var url=`http://ws.audioscrobbler.com/2.0/?method=track.search&api_key=920f792ee345290b28ca1b88329197b4&track=${value}&format=json&limit=10`;
+      var url=`http://ws.audioscrobbler.com/2.0/?method=track.search&api_key=d12367512494dccf2fdfc7bd75582cfd&track=${value}&format=json&limit=10`;
       return this.http.get(url);
     }
 
@@ -42,7 +42,7 @@ export class MusicService {
 
   getDetails(artist,name): any{
     console.log(artist, name)
-    var url = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=920f792ee345290b28ca1b88329197b4&artist=${artist}&album=${name}&format=json`
-  return this.http.get(url);
+    var url = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=d12367512494dccf2fdfc7bd75582cfd&artist=${artist}&album=${name}&format=json`
+    return this.http.get(url);
   }
 }
